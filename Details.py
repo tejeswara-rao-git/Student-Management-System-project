484e06000d8733df93308dfc350683a7ecc7c54e
tejeswara-rao-git <tejeswararaojami1@gmail.com>
2025-11-14 17:59:35 +0530

    Some changes

--- Details.py
+++ Details.py
@@ -1,5 +1,27 @@
-students = []
+# def load_students():
+#     students = []
 
+#     with open("student_details.txt","r") as f:
+#         for line in f:
+#             sID,name,marks = line.strip().split(',')
+#             students.append({"id": sID, "name": name, "marks": marks})
+#     return students
+
+
+# students = load_students()
+def load_students():
+    students = []
+    try:
+        with open("student_details.txt","r") as f:
+            for line in f:
+                sID, name, marks = line.strip().split(',')
+                students.append({"id": sID, "name": name, "marks": marks})
+    except FileNotFoundError:
+        pass  # if file doesn't exist, start with an empty list
+    return students
+
+
+students = load_students()
 
 def add_student():
     sid = input("Enter student ID :")
@@ -11,6 +33,7 @@ def add_student():
     students.append(student)
 
     print("Student added sucessfully ! \n")
+
 
 def view_student():
     # print("DEBUG: ", students)
@@ -71,6 +94,10 @@ def delete_student():
     if not found:
         print("Student id not found.\n")
 
+def save_students(students):
+    with open("student_details.txt","w") as f:
+        for s in students:
+            f.write(f"{s['id']},{s['name']},{s['marks']} \n")
 
 
 while True:
@@ -93,8 +120,10 @@ Enter your choice: """)
     elif(choice == "5"):
         delete_student()    
     elif(choice == "6"):
+        save_students(students)
         print("Exiting the program....")
         break
     else:
         print("Invalid choice. Please choose a correct choice \n")    
     
+save_students(students)
